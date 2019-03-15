58c3f880c8829a01cdac24b39bf686e44039dfa5
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-15 15:03:51 -0700

    getRandomAnswers inc/functions.php

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -10,8 +10,19 @@ function getCorrectAnswer($randomNumbers) {
   return array_sum($randomNumbers);
 }
 
-// Generate random questions
-// Loop for required number of questions
 // Get incorrect answers within 10 numbers either way of correct answer
 // Make sure it is a unique answer
+function getRandomAnswers($correctAnswer) {
+  $answersArray[] = $correctAnswer;
+  do {
+    $random = random_int($correctAnswer - 10, $correctAnswer + 10);
+    if ($random != $correctAnswer) {
+      $answersArray[] = $random;
+    }
+  } while (count($answersArray) <= 2);
+  return $answersArray;
+}
+
+// Generate random questions
+// Loop for required number of questions
 // Add question and answer to questions array
