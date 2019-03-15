cdb5539e81d1cdebf54819809bbec4b785ce3ca8
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-15 15:02:31 -0700

    getRandomNumbers and getCorrectAnswer inc/functions.php

--- inc/generate_questions.php
+++ inc/generate_questions.php
@@ -1,10 +1,17 @@
 <?php
 
+// Get random numbers to add
+function getRandomNumbers() {
+  return array(random_int(0,132), random_int(0,132));
+}
+
+// Calculate correct answer
+function getCorrectAnswer($randomNumbers) {
+  return array_sum($randomNumbers);
+}
 
 // Generate random questions
 // Loop for required number of questions
-// Calculate correct answer
 // Get incorrect answers within 10 numbers either way of correct answer
 // Make sure it is a unique answer
 // Add question and answer to questions array
-// Get random numbers to add -->
