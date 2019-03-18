f61f008782dbefdba2efb34e69aefd9a9fd305f4
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-03-17 20:18:05 -0700

    when  is empty session destroy

--- inc/quiz.php
+++ inc/quiz.php
@@ -6,3 +6,13 @@
 // Include questions
 include("generate_questions.php");
 session_start();
+
+// Show which question they are on
+$page = filter_input(INPUT_GET, 'p', FILTER_SANITIZE_NUMBER_INT);
+
+// end last session and build a new quiz
+if (empty($page)) {
+  session_destroy();
+  $page = 1;
+  writeQuiz();
+}
